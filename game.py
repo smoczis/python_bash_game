f619f160496e1ac5c9a836d92cac45d4a6664921
blazpie <blazej.pierzak@gmail.com>
2017-04-28 14:24:27 +0200

    maps made as class instances

--- game.py
+++ game.py
@@ -15,8 +15,8 @@ from bombs_specification import *
 
 COLOURS = {'X': '\x1b[0;31;41m'+'X'+'\x1b[0m', 'G': '\x1b[0;32;42m'+'G'+'\x1b[0m', 'N': '\x1b[0;34;44m'+'N'+'\x1b[0m'}
 BLOCKERS = [COLOURS['X'], COLOURS['G'], COLOURS['N']]
-board = []
-mines = []
+
+
 HINTS = {
         '0': [("Type the capital of Poland", "Warsaw"), ("Type the capital of France", "Paris"),
               ("Dupa cycki lasery", "Dzoana Krupa")],
@@ -104,63 +104,76 @@ def create_hero():
     return hero
 
 
-def put_mines(quantity):
-    """randomly selecting positions of given quantity of mines. returns a list of tuples (x, y)"""
-    mines = []
-    global board
-    while len(mines) < quantity:
-        y = random.randint(0, len(board)-1)
-        x = random.randint(0, len(board[0])-1)
-        if board[y][x] == ' ':
-            mines.append((x, y))
-    return mines
+class Mapa:
+    def __init__(self, plik):
+        self.board = []
+        self.mines = []
+        self.import_map(plik)
+        self.colour_map(self.board)
+        self.put_mines(20)
 
+    def put_mines(self, quantity):
+        """randomly selecting positions of given quantity of mines. returns a list of tuples (x, y)"""
+        while len(self.mines) < quantity:
+            y = random.randint(0, len(self.board)-1)
+            x = random.randint(0, len(self.board[0])-1)
+            if self.board[y][x] == ' ':
+                self.mines.append((x, y))
 
-def hide_mines():
+    def import_map(self, mapa):
+        with open(mapa, 'r', newline='\n') as map_file:
+            for line in map_file:
+                self.board.append([char for char in line[:-1]])
+
+    def colour_map(self, board):
+        COLOURS = {'X': '\x1b[0;31;41m'+'X'+'\x1b[0m',
+                   'G': '\x1b[0;32;42m'+'G'+'\x1b[0m',
+                   'N': '\x1b[0;34;44m'+'N'+'\x1b[0m'}
+        for line_i in range(len(self.board)):
+            for char_i in range(len(self.board[line_i])):
+                if self.board[line_i][char_i] in COLOURS.keys():
+                    self.board[line_i][char_i] = COLOURS[self.board[line_i][char_i]]
+
+def hide_mines(actual):
     """hiding all mines, that were printed before by show_neighbours()"""
-    global mines
-    global board
-    for line_i in range(len(board)):
-        for char_i in range(len(board[line_i])):
-            if board[line_i][char_i] == 'X':
-                board[line_i][char_i] = ' '
-    return board
+    for line_i in range(len(actual.board)):
+        for char_i in range(len(actual.board[line_i])):
+            if actual.board[line_i][char_i] == 'X':
+                actual.board[line_i][char_i] = ' '
+    return actual.board
 
 
-def calc_neighbours(board, x, y, distance=1):
+def calc_neighbours(actual, x, y, distance=1):
     """calculating and set to list all board cells in given distance (default 1) from x, y"""
     neighbours = []
     for delta_y in range(-distance, distance + 1):
         for delta_x in range(-distance, distance + 1):
-            if y + delta_y in range(len(board)) and x + delta_x in range(len(board[0])):
+            if y + delta_y in range(len(actual.board)) and x + delta_x in range(len(actual.board[0])):
                 neighbours.append((x + delta_x, y + delta_y))
     return neighbours
 
 
-def show_neighbours(neighbours):
+def show_neighbours(actual, neighbours):
     """showing on board all hidden objects in distance
        (calculated by calc_neighbours) of player's position. return board"""
-    global mines
-    global board
     for (x, y) in neighbours:
-        if (x, y) in mines:
-            board[y][x] = "X"
-    return board
+        if (x, y) in actual.mines:
+            actual.board[y][x] = "X"
+    return actual.board
 
 
-def insert_player(x, y, detector=False):
+def insert_player(actual, x, y, detector=False):
     """inserting player on board on given position. in future, it may get players atributes, to diverse result"""
     global COLOURS
-    global board
     if detector:
         see_distance = 5
     else:
         see_distance = 1
-    hide_mines()
-    neighbours = calc_neighbours(board, x, y, see_distance)
-    show_neighbours(neighbours)
-    board[y][x] = "@"
-    return board
+    hide_mines(actual)
+    neighbours = calc_neighbours(actual, x, y, see_distance)
+    show_neighbours(actual, neighbours)
+    actual.board[y][x] = "@"
+    return actual.board
 
 
 def getch():
@@ -175,33 +188,39 @@ def getch():
     return ch.lower()
 
 
-def move(x, y):
+def react(actual, x, y):
+    for mine in actiual.mines:
+        if mine in calc_neighbours(x, y):
+            actiual.mines.remove(mine)
+
+
+def move(actual, x, y):
     """moving player basing on previous position on board, using getch()"""
     global BLOCKERS
-    global board
     key = getch()
-    board[y][x] = " "
-    if key == "a" and board[y][x-1] not in BLOCKERS:
+    actual.board[y][x] = " "
+    if key == "a" and actual.board[y][x-1] not in BLOCKERS:
         x -= 1
-    elif key == "d" and board[y][x+1] not in BLOCKERS:
+    elif key == "d" and actual.board[y][x+1] not in BLOCKERS:
         x += 1
-    elif key == "w" and board[y-1][x] not in BLOCKERS:
+    elif key == "w" and actual.board[y-1][x] not in BLOCKERS:
         y -= 1
-    elif key == "s" and board[y+1][x] not in BLOCKERS:
+    elif key == "s" and actual.board[y+1][x] not in BLOCKERS:
         y += 1
     elif key == "h":
-        show_pop_up(HINTS)
+        show_pop_up(actual, HINTS)
     elif key == "m":
         sys.exit()
+    elif key == "e":
+        react(actual, x, y)
     else:
         pass
     return x, y
 
 
-def show_pop_up(dictionary, level='0'):
-    global board
+def show_pop_up(actual, dictionary, level='0'):
     board_copy = []
-    for item in board:
+    for item in actual.board:
         board_copy.append(item[:])
     show_hint = choice(dictionary['0'])
     pop_height, pop_width = 5, len(show_hint[0])+4
@@ -216,23 +235,20 @@ def show_pop_up(dictionary, level='0'):
         else:
             board_copy[15+line][x_start:x_end] = ["#"] + [" "]*2 + help_list + [" "]*2 + ["#"]
     print_board(board_copy)
-    sleep(2)
+    sleep(1)
 
 
 def main():
-    global mines
-    global board
     height, width = 39, 100
-    board = import_map('map1.txt')
-    mines = put_mines(20)
+    actual = Mapa('map1.txt')
     pos = 36, 13
     hero = create_hero()
-    insert_player(pos[0], pos[1])
-    print_board(board)
+    insert_player(actual, pos[0], pos[1])
+    print_board(actual.board)
     while True:
-        pos = move(pos[0], pos[1])
-        insert_player(pos[0], pos[1], detector=True)
-        print_board(board)
+        pos = move(actual, pos[0], pos[1])
+        insert_player(actual, pos[0], pos[1], detector=True)
+        print_board(actual.board)
 
 
 if __name__ == '__main__':
