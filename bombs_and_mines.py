74ef83564a021bc01bd71451eaa42fb5a41cbabf
blazpie <blazej.pierzak@gmail.com>
2017-05-09 20:37:42 +0200

    kosmetyczne popraweczki, nic sie tu nie zmienilo

--- bombs_and_mines.py
+++ bombs_and_mines.py
@@ -1,11 +1,11 @@
 import random
 
 
-nuke_bomb = { 'exp_require': 10000,
-              'wisdom_required': 5 + random.randint(1,10),
-              'mental_strength_required': 10
-              'code_required': 'hero goes secret code',
-              'count_down': 60 }
+nuke_bomb = {'exp_require': 10000,
+             'wisdom_required': 5 + random.randint(1, 10),
+             'mental_strength_required': 10
+             'code_required': 'hero goes secret code',
+             'count_down': 60}
 
 hidden_mine = {}
 hidden_mine2 = {}
