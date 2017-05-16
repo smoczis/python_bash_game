255da471d65e166279849c2fadb406f4b0fb2eed
smoczis <romanowski.mateusz93@gmail.com>
2017-05-16 14:41:03 +0200

    w8ing 4 popup func

--- hot_cold_game.py
+++ hot_cold_game.py
@@ -1,21 +1,19 @@
 """
-develop hotcold game using popup window
 start menue
 win/lose screen
-chest on classes
 map!!!
 
 """
 from random import randint, choice
 
 
-def generate_number(binary=False, even=False):
+def generate_number(number_length=3, binary=False, even=False):
     random_number = []
     digits = 9
     if binary:
         digits = 1
     even_nums = [0, 2, 4, 6, 8]
-    while len(random_number) != 3:
+    while len(random_number) != number_length:
         if even:
             num = choice(even_nums)
             if num not in random_number:
@@ -25,24 +23,22 @@ def generate_number(binary=False, even=False):
             if num not in random_number:
                 random_number.append(str(num))
     return random_number
-# random_number = ''.join(random_number)
 
 
-def guessing_number(random_number, attempts):
+def guessing_number(random_number, attempts, number_length=3):
+    printing_result = []
     while attempts:
-        printing_result = []
         pop_up_list = ['You got {} attempts'.format(attempts), '\n', *printing_result]
         print (*pop_up_list)
-        guess_number = input("pick 3 digit number: ")
+        guess_number = input("pick {} digit number: ".format(number_length))
 
-        while not guess_number.isdigit() or len(guess_number) != 3:
+        while not guess_number.isdigit() or len(guess_number) != number_length:
             guess_number = input("It is not an integer!")
 
         # print ("You got {} attempts".format(attempts))
         guess_number = list(guess_number)
         # print (guess_number)
-
-
+        printing_result = []
         for i, elem in enumerate(guess_number):
             if elem in random_number:
                 if elem == random_number[i]:
@@ -51,27 +47,21 @@ def guessing_number(random_number, attempts):
                     printing_result.append('warm')
 
         if not printing_result:
-            print ("cold")
+            printing_result = ['cold']
 
-        print (*printing_result)
         if all([i == 'hot' for i in printing_result]) and len(printing_result) == 3:
-            print ('cool')
+            pop_up_list = ['You guessed the number']
             break
         attempts -= 1
     else:
         pop_up_list = ['You lose']
     return pop_up_list
-    '''
-     goal = "".join(printing_result)
-    if goal == "hothothot":
-        print ("Cool")
-        break
-        '''
 
 
 def main():
-    number = generate_number(even=True)
-    print (guessing_number(number, 10))
+    number = generate_number(number_length=5)
+    print (number)
+    print (guessing_number(number, 10, number_length=5))
 
 
 if __name__ == "__main__":
