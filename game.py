3c7adb3c9e7df7cc63eff7e266e1436d29bc8908
smoczis <romanowski.mateusz93@gmail.com>
2017-05-18 16:27:34 +0200

    added highscores function

--- game.py
+++ game.py
@@ -28,21 +28,47 @@ def action(player, key):
     return game_on
 
 
-def end_game(board):
-    pop_up(board, ['HIGHSCORES'], auto_hide=2)
+def end_game(board, name, game_time, score):
+    saved_scores = open('texts/scores.txt').readlines()
+    new_score = "{} - {} - {}\n".format(score, game_time, name)
+    if not saved_scores:
+        saved_scores.append(new_score)
+    else:
+        i = 0
+        new_index = len(saved_scores)
+        for line in saved_scores:
+            line_elem = line.strip()
+            line_elem = line_elem.split(' - ')
+            if score >= int(line_elem[0]):
+                new_index = i
+                break
+            i += 1
+        saved_scores.insert(new_index, new_score)
+    with open("texts/scores.txt", 'w') as fw:
+        iterator = 0
+        while iterator < 10 and iterator < len(saved_scores):
+            fw.write(saved_scores[iterator])
+            iterator += 1
+    score_list = open('texts/scores.txt').readlines()
+    score_list.insert(0, 'HIGHSCORES')
+    score_list.insert(1, 'SCORE - GAME TIME - NAME')
+    pop_up(board, score_list, auto_hide=4)
     os.system('clear')
 
 
 def main():
     player = Hero()
     game_on = True
+    time_start = time.time()
     player.insert_on_board()
     while player.alive and game_on:
         print_board(player.place.board, player)
         key = getch().lower()
         game_on = action(player, key)
         player.insert_on_board()
-    end_game(player.place.board)
+    game_time = round(time.time() - time_start)
+    final_score = 1000 - game_time
+    end_game(player.place.board, player.name, game_time, final_score)
 
 
 if __name__ == '__main__':
